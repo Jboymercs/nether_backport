6251aa63bcfd760b65da559f730faee2ac345f66
Sir Squidly <sir.squildy.le.third@gmail.com>
2024-10-13 20:42:54 -0700

    Added Nylium Bonemealing
    + Nylium can be Bonemealed to grow random Nether flora

--- src/main/java/com/unseen/nb/common/blocks/BlockNetherGrass.java
+++ src/main/java/com/unseen/nb/common/blocks/BlockNetherGrass.java
@@ -1,6 +1,7 @@
 package com.unseen.nb.common.blocks;
 
 import com.unseen.nb.common.blocks.base.BlockBase;
+import com.unseen.nb.init.ModBlocks;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -48,14 +49,74 @@ public class BlockNetherGrass extends BlockBase implements IGrowable
 
     @Override
     public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient)
-    { return true; }
+    { return worldIn.isAirBlock(pos.up()); }
 
     @Override
     public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state)
-    { return true; }
+    { return worldIn.isAirBlock(pos.up()); }
 
-    /** TODO: Add logic for Nether plant generation, requires research */
     @Override
     public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state)
-    {}
+    {
+        BlockPos blockpos = pos.up();
+        boolean additionalTwistingVines = rand.nextInt(8) == 0;
+
+        for (int i = 0; i < 64; ++i)
+        {
+            BlockPos blockpos1 = blockpos;
+            int j = 0;
+
+            /* Detects what plants to place based on the current position's below one. */
+            boolean isWarped = worldIn.getBlockState(blockpos1.down()).getBlock() == ModBlocks.WARPED_GRASS;
+
+            while (true)
+            {
+                if (j >= i / 16)
+                {
+                    if (worldIn.isAirBlock(blockpos1))
+                    {
+                        /* We use a random number between 1 and 100 when deciding what to place. */
+                        int dice = rand.nextInt(101);
+
+                        if (isWarped)
+                        {
+                            if (dice <= 20)
+                            { worldIn.setBlockState(blockpos1, ModBlocks.WARPED_SPROUT.getDefaultState(), 3); }
+                            else if (dice <= 85)
+                            { worldIn.setBlockState(blockpos1, ModBlocks.WARPED_ROOTS.getDefaultState(), 3); }
+                            else if (dice <= 98)
+                            { worldIn.setBlockState(blockpos1, ModBlocks.WARPED_FUNGUS.getDefaultState(), 3); }
+                            else if (dice <= 99)
+                            { worldIn.setBlockState(blockpos1, ModBlocks.CRIMSON_ROOTS.getDefaultState(), 3); }
+                            else
+                            { worldIn.setBlockState(blockpos1, ModBlocks.CRIMSON_FUNGUS.getDefaultState(), 3); }
+
+                            if (additionalTwistingVines)
+                            {
+                                worldIn.setBlockState(blockpos1, ModBlocks.WARPED_VINES.getDefaultState(), 3);
+                                additionalTwistingVines = false;
+                            }
+                        }
+                        else
+                        {
+                            if (dice <= 87)
+                            { worldIn.setBlockState(blockpos1, ModBlocks.CRIMSON_ROOTS.getDefaultState(), 3); }
+                            else if (dice <= 98)
+                            { worldIn.setBlockState(blockpos1, ModBlocks.CRIMSON_FUNGUS.getDefaultState(), 3); }
+                            else
+                            { worldIn.setBlockState(blockpos1, ModBlocks.WARPED_FUNGUS.getDefaultState(), 3); }
+                        }
+                    }
+                    break;
+                }
+
+                blockpos1 = blockpos1.add(rand.nextInt(3) - 1, (rand.nextInt(3) - 1) * rand.nextInt(3) / 2, rand.nextInt(3) - 1);
+
+                if (worldIn.getBlockState(blockpos1.down()).getBlock() != ModBlocks.CRIMSON_GRASS && worldIn.getBlockState(blockpos1.down()).getBlock() != ModBlocks.WARPED_GRASS || worldIn.getBlockState(blockpos1).isNormalCube())
+                { break; }
+
+                ++j;
+            }
+        }
+    }
 }
