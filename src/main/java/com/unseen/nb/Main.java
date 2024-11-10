6f9f73dd8cbd1bf3f490d4caa9d6071669d8dae8
Szymon Hajbowicz <szymon.hajbowicz@gmail.com>
2024-11-10 15:02:39 +0100

    Fix NetherAPI id and add entry to @Mod annotation to display missing mods screen

--- src/main/java/com/unseen/nb/Main.java
+++ src/main/java/com/unseen/nb/Main.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ModReference.MOD_ID, name = ModReference.NAME, version = ModReference.VERSION)
+@Mod(modid = ModReference.MOD_ID, name = ModReference.NAME, version = ModReference.VERSION, dependencies = "required-before:nether_api")
 public class Main {
 
     @SidedProxy(clientSide = ModReference.CLIENT_PROXY_CLASS, serverSide = ModReference.COMMON_PROXY_CLASS)
